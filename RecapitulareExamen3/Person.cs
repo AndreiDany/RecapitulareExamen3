e9418e2711ef4b9a422be00ded308f18a85bc68e
Daniel Leon Andrei <daniel.andrei@emmanuel.ro>
2022-02-05 20:34:36 +0200

    Hello

---
+++ RecapitulareExamen3/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapitulareExamen3
+{
+    public class Person
+    {
+        public Person(string Nume, string Prenume)
+        {
+            this.Nume = Nume;
+            this.Prenume = Prenume;
+        }
+
+        public string Nume;
+        public string Prenume;
+        public string nume {
+            get { return this.Nume; }
+            set { this.Nume = value ?? "Andrei"; }
+        }
+        
+        public string prenume
+        {
+            get { return this.Prenume; }
+            set { this.Prenume = value ?? "Daniel"; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{this.nume} {this.prenume}");
+        }
+
+        public static void Print1(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ RecapitulareExamen3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RecapitulareExamen3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Andrei","Daniel");
+            Console.WriteLine(person.Nume);
+            person.Nume = "Dani";
+            person.Print();
+            Person.Print1("Hello!");
+
+        }
+    }
+}
